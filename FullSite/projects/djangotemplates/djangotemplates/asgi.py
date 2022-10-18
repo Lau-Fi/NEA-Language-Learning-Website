95521ae7f7bd746d868311bc4a00e7e31f250759
Laurence <starryflight@outlook.com>
2022-10-18 21:31:30 +0100

    Workable text on a chatroom page!

--- FullSite/projects/djangotemplates/djangotemplates/asgi.py
+++ FullSite/projects/djangotemplates/djangotemplates/asgi.py
@@ -19,13 +19,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-import chat.routing
+import example.routing
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+            AuthMiddlewareStack(URLRouter(example.routing.websocket_urlpatterns))
         ),
     }
 )
