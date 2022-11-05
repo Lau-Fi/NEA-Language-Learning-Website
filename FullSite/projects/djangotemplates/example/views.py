95dcc58275d071bf88e888511ac62b353e7e6017
Laurence <starryflight@outlook.com>
2022-11-05 13:25:42 +0000

    login/sign up page early steps

--- FullSite/projects/djangotemplates/example/views.py
+++ FullSite/projects/djangotemplates/example/views.py
@@ -1,7 +1,9 @@
 # djangotemplates/example/views.py
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView # Import TemplateView
 from .models import Question
+from django.contrib.auth.models import User, auth
 import random 
 
 def practicehtml(request):
@@ -25,6 +27,59 @@ def room(request, room_name):
 
 def home(request):
     return render (request, "index.html" ) 
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password==confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already taken')
+                return redirect(register)
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email is already taken')
+                return redirect(register)
+            else:
+                user = User.objects.create_user(username=username, password=password, 
+                                        email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                
+                return redirect('login_user')
+
+
+        else:
+            messages.info(request, 'Both passwords are not matching')
+            return redirect(register)
+            
+
+    else:
+        return render(request, 'registeration.html')
+
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Invalid Username or Password')
+            return redirect('login_user')
+    
+    else:
+        return render(request, 'login.html')
+
 # Add the two views we have been talking about  all this time :)
 class HomePageView(TemplateView):
     template_name = "index.html"
