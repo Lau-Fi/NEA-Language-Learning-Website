bb32656ccfc8eb8fb43a860b19eb2593de7ad43c
Laurence <starryflight@outlook.com>
2022-12-04 13:27:20 +0000

    working updated and - mostly validated code

--- FullSite/projects/djangotemplates/example/views.py
+++ FullSite/projects/djangotemplates/example/views.py
@@ -113,10 +113,10 @@ def login_user(request):
     else:
         return render(request, 'login.html')
 
-
+@login_required
 def update_user(request):
     if request.method == 'POST':
-        profile = Profile.objects.get(user = request.user)
+        userprofile = Profile.objects.get(user = request.user)
         user = request.user
         first_name = request.POST.get('first_name',None)
         last_name = request.POST.get('last_name',None) 
@@ -127,28 +127,55 @@ def update_user(request):
         bio = request.POST.get('bio',None) 
         picture = request.POST.get('picture',None)
         
+
         if bio is not None:
-            profile.bio = bio 
+            userprofile.bio = bio 
         if picture is not None:
-            profile.picture = picture
+            userprofile.picture = picture
         if first_name is not None:
-            user.first_name = first_name
+            if first_name and first_name.strip():
+                user.first_name = first_name
+            else:
+                messages.info(request, 'Please enter a First Name')
+                return redirect(update_user)
         if last_name is not None:
-            user.last_name = last_name 
+            if last_name and last_name.strip():
+                user.last_name = last_name
+            else:
+                messages.info(request, 'Please enter a Last Name')
+                return redirect(update_user) 
         if username is not None:
-            user.username = username
+            if username and username.strip():
+                if User.objects.filter(username=username).exists():
+                    messages.info(request, 'Username is already taken')
+                    return redirect(update_user)
+                user.username = username
+            else:
+                messages.info(request, 'Please enter a Username')
+                return redirect(update_user)
         if email is not None:
-            user.email = email
-        if password is not None:
-            user.password = password 
-        if confirm_password is not None:
-            user.confirm_password = confirm_password
-        
+            if email and email.strip():
+                user.email = email
+            else:
+                messages.info(request, 'Please enter an email')
+                return redirect(update_user)
+        if password is not None or confirm_password is not None:
+            if password and confirm_password and password.strip() and confirm_password.strip():
+                if password==confirm_password:
+                    user.set_password(password) 
+                else:
+                    messages.info(request, 'Passwords do not match')
+                    return redirect(update_user)
+            else:
+                messages.info(request, 'Please enter your passwords')
+                return redirect(update_user)
+
         user.save()
-        profile.save()
-        return redirect('profile.html')
+        userprofile.save()
+        auth.login(request, user)
+        return redirect(profile)
     else:
-        return redirect('index')
+        return render(request, 'profile.html')
        
 
 def logout_user(request):
@@ -190,4 +217,15 @@ class login(TemplateView):
     template_name = "login.html"
 
 
-                                                                        
+# We need to make sure updated passwords and encrypted and add validation
+# to the update page so if a username is the same as one already in the 
+# database it will not allow it. Futhermore, we may need to change the 
+# update form as there are many errors.
+# You can set nothing as your password. 
+# Account dropsdown on every page
+# Picture disappears when updating         
+# Logging me out when updating user profile? 
+# Required redirect - the redirect is the wrong URL 
+# disallow blank passwords or preferably passwords less than 8 characters 
+# Need to change from is none checks 
+# Need to find out that blank entries in the profile count as none (does not turn up) or blank strings = IT IS A BLANK STRING                               
